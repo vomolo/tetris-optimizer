b342c98e6c315663b41c8c0020c25074e1fe34b0
vomolo <valentineomolo1@gmail.com>
2025-05-07 22:29:57 +0300

    REFINE: Update code to join types and validator

--- main.go
+++ main.go
@@ -8,18 +8,16 @@ import (
 
 func main() {
 	if len(os.Args) != 2 {
-		fmt.Fprintln(os.Stderr, "Usage: optimizer <filename>")
+		fmt.Fprintln(os.Stderr, "Usage: go run main.go <filename>")
 		os.Exit(1)
 	}
 
-	// Get both the formatted output and validation error
-	output, err := functions.Validate(os.Args[1])
+	solution, err := functions.Validate(os.Args[1])
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Validation error: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
 		os.Exit(1)
 	}
 
-	// Print the formatted output with letter replacements
-	fmt.Println("Valid tetromino configuration:")
-	fmt.Print(output)
+	fmt.Println("Solution:")
+	fmt.Println(solution)
 }
