377f4b4af4fba775e699cb7306f181911f993e81
vomolo <valentineomolo1@gmail.com>
2025-05-09 18:46:35 +0300

    REFINE: Change exit codes to 0 for successful termination in main function and avoid printing "exit status 1"

--- main.go
+++ main.go
@@ -9,13 +9,13 @@ import (
 func main() {
 	if len(os.Args) != 2 {
 		fmt.Fprintln(os.Stderr, "Usage: go run main.go <filename>")
-		os.Exit(1)
+		os.Exit(0)
 	}
 
 	solution, err := solver.Validate(os.Args[1])
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "ERROR")
-		os.Exit(1)
+		os.Exit(0)
 	}
 
 	fmt.Println(solution)
