3a367e93e6aacf9081b681ca2643578788f24a18
vomolo <valentineomolo1@gmail.com>
2025-05-09 15:24:54 +0300

    REFINE: Rename ValidationError to validationError for consistency

--- internal/solver/errors.go
+++ internal/solver/errors.go
@@ -1,15 +1,13 @@
 package solver
 
-type ValidationError struct {
-	message string
+type validationError struct {
+    message string
 }
 
-func (e *ValidationError) Error() string {
-	return e.message
+func (e *validationError) Error() string {
+    return e.message
 }
 
 func newValidationError(message string) error {
-	return &ValidationError{
-		message: message,
-	}
+    return &validationError{message: message}
 }
