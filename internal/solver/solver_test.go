5dbc8e1e2bf67d2bd4d2245d24a1928bb2d203dc
vomolo <valentineomolo1@gmail.com>
2025-05-12 11:44:29 +0300

    ADD: Implement TestSolveTetrominos to validate tetromino solving logic

--- internal/solver/solver_test.go
+++ internal/solver/solver_test.go
@@ -26,3 +26,72 @@ func TestMin(t *testing.T) {
 	}
 }
 
+func createTetromino(letter rune, points [][2]int, width, height int) *Tetromino {
+	t := &Tetromino{
+		Letter: letter,
+		Width:  width,
+		Height: height,
+	}
+	for _, p := range points {
+		t.Points = append(t.Points, Point{X: p[0], Y: p[1]})
+	}
+	return t
+}
+
+func TestSolveTetrominos(t *testing.T) {
+	tests := []struct {
+		name       string
+		tetrominos []*Tetromino
+		wantErr    bool
+	}{
+		{
+			name:       "empty input",
+			tetrominos: []*Tetromino{},
+			wantErr:    true,
+		},
+		{
+			name: "single square tetromino",
+			tetrominos: []*Tetromino{
+				createTetromino('A', [][2]int{{0, 0}, {1, 0}, {0, 1}, {1, 1}}, 2, 2),
+			},
+			wantErr: false,
+		},
+		{
+			name: "two simple tetrominos",
+			tetrominos: []*Tetromino{
+				createTetromino('A', [][2]int{{0, 0}, {1, 0}, {0, 1}, {1, 1}}, 2, 2),
+				createTetromino('B', [][2]int{{0, 0}, {0, 1}, {0, 2}, {1, 2}}, 2, 3),
+			},
+			wantErr: false,
+		},
+		{
+			name: "truly impossible configuration",
+			tetrominos: []*Tetromino{
+				createTetromino('A', [][2]int{{0, 0}, {1, 0}, {2, 0}, {3, 0}}, 4, 1),
+				createTetromino('B', [][2]int{{0, 0}, {1, 0}, {2, 0}, {3, 0}}, 4, 1),
+				createTetromino('C', [][2]int{{0, 0}, {1, 0}, {2, 0}, {3, 0}}, 4, 1),
+				createTetromino('D', [][2]int{{0, 0}, {1, 0}, {2, 0}, {3, 0}}, 4, 1),
+				createTetromino('E', [][2]int{{0, 0}, {1, 0}, {2, 0}, {3, 0}}, 4, 1),
+			},
+			wantErr: false,
+		},
+		{
+			name: "complex possible configuration",
+			tetrominos: []*Tetromino{
+				createTetromino('A', [][2]int{{0, 0}, {1, 0}, {1, 1}, {2, 1}}, 3, 2),
+				createTetromino('B', [][2]int{{0, 1}, {1, 1}, {1, 0}, {2, 0}}, 3, 2),
+				createTetromino('C', [][2]int{{0, 0}, {0, 1}, {0, 2}, {1, 2}}, 2, 3),
+			},
+			wantErr: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := SolveTetrominos(tt.tetrominos)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("%s: SolveTetrominos() error = %v, wantErr %v", tt.name, err, tt.wantErr)
+			}
+		})
+	}
+}
