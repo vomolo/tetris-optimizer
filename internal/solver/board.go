e56e17e60890b7c19e7de079b125ef11fa271b5f
vomolo <valentineomolo1@gmail.com>
2025-05-19 23:31:29 +0300

    REFINE: Simplify Board structure by consolidating width and height into a single size parameter

--- internal/solver/board.go
+++ internal/solver/board.go
@@ -1,62 +1,31 @@
 package solver
 
-import "bytes"
-
+// Board represents the Tetris game board.
 type Board struct {
 	Grid   [][]rune
-	Width  int
-	Height int
+	Size   int // Square board, so width = height
 	Placed int
 }
 
-func NewBoard(width, height int) *Board {
-	if width <= 0 || height <= 0 {
+// NewBoard creates a new square board of given size.
+func NewBoard(size int) *Board {
+	if size <= 0 {
 		return nil
 	}
-	grid := make([][]rune, height)
+	grid := make([][]rune, size)
 	for i := range grid {
-		grid[i] = make([]rune, width)
+		grid[i] = make([]rune, size)
 	}
-	return &Board{Grid: grid, Width: width, Height: height}
+	return &Board{Grid: grid, Size: size}
 }
 
-func (b *Board) canPlace(t *Tetromino, x, y int) bool {
+// CanPlace checks if a tetromino can be placed at position (x, y).
+func (b *Board) CanPlace(t *Tetromino, x, y int) bool {
 	for _, p := range t.Points {
 		nx, ny := x+p.X, y+p.Y
-		if nx < 0 || ny < 0 || nx >= b.Width || ny >= b.Height || b.Grid[ny][nx] != 0 {
+		if nx < 0 || ny < 0 || nx >= b.Size || ny >= b.Size || b.Grid[ny][nx] != 0 {
 			return false
 		}
 	}
 	return true
-}
-
-func (b *Board) place(t *Tetromino, x, y int) {
-	for _, p := range t.Points {
-		b.Grid[y+p.Y][x+p.X] = t.Letter
-	}
-	b.Placed++
-}
-
-func (b *Board) remove(t *Tetromino, x, y int) {
-	for _, p := range t.Points {
-		b.Grid[y+p.Y][x+p.X] = 0
-	}
-	b.Placed--
-}
-
-func boardToString(b *Board) string {
-	var buf bytes.Buffer
-	for y := range b.Height {
-		for x := range b.Width {
-			if b.Grid[y][x] == 0 {
-				buf.WriteByte('.')
-			} else {
-				buf.WriteRune(b.Grid[y][x])
-			}
-		}
-		if y < b.Height-1 {
-			buf.WriteByte('\n')
-		}
-	}
-	return buf.String()
 }
