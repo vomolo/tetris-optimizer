3327d17b4b75667cbbfa2ae2ddb86286b7fba180
vomolo <valentineomolo1@gmail.com>
2025-05-19 23:46:02 +0300

    REFINE: Implement general square solver and enhance tetromino grouping logic

--- internal/solver/solver.go
+++ internal/solver/solver.go
@@ -3,6 +3,7 @@ package solver
 import (
 	"fmt"
 	"math"
+	"sort"
 )
 
 const (
@@ -100,4 +101,94 @@ func solveWithoutRotation(tetrominos []*Tetromino, index int, board *Board) (*Bo
 	}
 
 	return "", fmt.Errorf("no optimized square solution found")
+}
+
+func generalSquareSolver(tetrominos []*Tetromino) (string, error) {
+	// Sort tetrominos by size and complexity
+	sortedTetrominos := make([]*Tetromino, len(tetrominos))
+	copy(sortedTetrominos, tetrominos)
+	sort.Slice(sortedTetrominos, func(i, j int) bool {
+		areaI := sortedTetrominos[i].Width * sortedTetrominos[i].Height
+		areaJ := sortedTetrominos[j].Width * sortedTetrominos[j].Height
+		if areaI != areaJ {
+			return areaI > areaJ
+		}
+		return calculateComplexity(sortedTetrominos[i]) > calculateComplexity(sortedTetrominos[j])
+	})
+
+	totalBlocks := len(tetrominos) * 4
+	minSize := int(math.Ceil(math.Sqrt(float64(totalBlocks))))
+
+	// Try solving with increasing square board sizes
+	for size := minSize; size <= minSize+5; size++ {
+		board := NewBoard(size)
+		if board == nil {
+			continue
+		}
+		if solve(board, sortedTetrominos, 0) {
+			return board.String(), nil
+		}
+	}
+	return "", fmt.Errorf("no square solution found")
+}
+
+func solve(board *Board, tetrominos []*Tetromino, index int) bool {
+	if index == len(tetrominos) {
+		return true
+	}
+
+	t := tetrominos[index]
+	for y := 0; y <= board.Size-t.Height; y++ {
+		for x := 0; x <= board.Size-t.Width; x++ {
+			if !board.CanPlace(t, x, y) {
+				continue
+			}
+
+			board.Place(t, x, y)
+			if solve(board, tetrominos, index+1) {
+				return true
+			}
+			board.Remove(t, x, y)
+		}
+	}
+	return false
+}
+
+type tetrominoGroup struct {
+	tetrominos []*Tetromino
+	points     []Point
+}
+
+func groupRepetitiveTetrominos(tetrominos []*Tetromino) []tetrominoGroup {
+	groups := make([]tetrominoGroup, 0)
+	used := make(map[int]bool)
+
+	for i, t1 := range tetrominos {
+		if used[i] {
+			continue
+		}
+		group := tetrominoGroup{tetrominos: []*Tetromino{t1}, points: t1.Points}
+		for j := i + 1; j < len(tetrominos); j++ {
+			if !used[j] && areTetrominosEqual(t1, tetrominos[j]) {
+				group.tetrominos = append(group.tetrominos, tetrominos[j])
+				used[j] = true
+			}
+		}
+		groups = append(groups, group)
+	}
+	return groups
+}
+
+func areTetrominosEqual(t1, t2 *Tetromino) bool {
+	if len(t1.Points) != len(t2.Points) {
+		return false
+	}
+	norm1 := normalizeTetromino(t1)
+	norm2 := normalizeTetromino(t2)
+	for i := range norm1 {
+		if norm1[i] != norm2[i] {
+			return false
+		}
+	}
+	return true
 }
